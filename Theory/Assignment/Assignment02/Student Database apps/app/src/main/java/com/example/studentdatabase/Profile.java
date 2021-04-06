b913f434ad3292fe9a02ed77c525b24d9247db7e
saiful Islam <58458609+Saiful1721648@users.noreply.github.com>
2021-04-07 02:17:23 +0600

    #15 Student database apps (Add Student information)

--- Theory/Assignment/Assignment02/Student Database apps/app/src/main/java/com/example/studentdatabase/Profile.java
+++ Theory/Assignment/Assignment02/Student Database apps/app/src/main/java/com/example/studentdatabase/Profile.java
@@ -1,13 +1,56 @@
 package com.example.studentdatabase;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Profile extends AppCompatActivity {
+public class Profile extends AppCompatActivity implements OnClickListener {
+
+    private EditText name,phone,emailAdress,studentId,presentAddress,permanentAddress,birthDay ;
+    private Button addinfo,skippage;
+    private Spinner schoolName, departmentName ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        name=findViewById(R.id.editTextTextPersonName);
+        phone=findViewById(R.id.editTextPhone);
+        emailAdress=findViewById(R.id.editTextTextEmailAddress);
+        studentId=findViewById(R.id.studentid);
+        presentAddress=findViewById(R.id.presentAddressText);
+        permanentAddress=findViewById(R.id.Permanent_address_id);
+        birthDay=findViewById(R.id.Birth_Date);
+        addinfo=findViewById(R.id.profile_save_btn);
+        skippage=findViewById(R.id.skip_btn_btn);
+        schoolName=findViewById(R.id.spinner);
+        departmentName=findViewById(R.id.spinner2);
+        
+    }
+
+    @Override
+    public void onClick(View v) {
+            String name1 =name.getText().toString();
+        String phone1 =phone.getText().toString();
+        String emailadress1 =emailAdress.getText().toString();
+        String studentid1 =studentId.getText().toString();
+        String presentadress1 =presentAddress.getText().toString();
+        String permanentadress1 =permanentAddress.getText().toString();
+        String birthDay1=birthDay.getText().toString();
+        addinfo.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Profile.this, "name"+ name1, Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+
+
     }
 }
