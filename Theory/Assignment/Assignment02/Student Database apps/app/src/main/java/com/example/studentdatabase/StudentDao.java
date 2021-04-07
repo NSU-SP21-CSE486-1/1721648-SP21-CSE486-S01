2b5dc31897896bbf713836d3c3896a91c0c4bc94
saiful Islam <58458609+Saiful1721648@users.noreply.github.com>
2021-04-07 18:40:42 +0600

    Revert "Revert "Revert "Revert "#4 Lab Assignment 1""""
    This reverts commit 17f0052a1ee3665482d6452b8eef92eae639cb11.

---
+++ Theory/Assignment/Assignment02/Student Database apps/app/src/main/java/com/example/studentdatabase/StudentDao.java
@@ -0,0 +1,12 @@
+package com.example.studentdatabase;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+
+@Dao
+public interface StudentDao {
+    @Insert
+    default Long insertTask(Student student) {
+        return null;
+    }
+}
