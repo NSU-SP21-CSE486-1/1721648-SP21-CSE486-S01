1eb4e42e16f1689d108502e66140a072329f95e5
saiful Islam <58458609+Saiful1721648@users.noreply.github.com>
2021-05-13 01:25:44 +0600

    #17 splash screen

---
+++ Project/Version01/NSU CPC APPS project/app/src/main/java/com/example/nsucpc/splashScreen.java
@@ -0,0 +1,30 @@
+package com.example.nsucpc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+public class splashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable(){
+
+                                @Override
+                                public void run() {
+                                    startActivity(new Intent(splashScreen.this,MainActivity.class));
+                                    finish();
+
+                                }
+                            },3000
+        
+        );
+        Toast.makeText(this,"Welcome to NSU CPC",Toast.LENGTH_SHORT).show();
+    }
+}
