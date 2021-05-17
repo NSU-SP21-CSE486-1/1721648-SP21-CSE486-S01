c4c131ff914737f4ab8e98716241a4e1e8a2f31a
saiful Islam <58458609+Saiful1721648@users.noreply.github.com>
2021-05-18 04:18:20 +0600

    #37 Student Log in

--- Project/Version01/NSU CPC APPS project/app/src/main/java/com/example/nsucpc/MemberStudentPortal.java
+++ Project/Version01/NSU CPC APPS project/app/src/main/java/com/example/nsucpc/MemberStudentPortal.java
@@ -2,13 +2,28 @@ package com.example.nsucpc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MemberStudentPortal extends AppCompatActivity {
+    ImageButton studentPortal , cpcMemberPortal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_student_portal);
+        studentPortal=findViewById(R.id.studentportalbtn);
+        cpcMemberPortal=findViewById(R.id.cpcMemberPortalButton);
+        studentPortal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MemberStudentPortal.this,StudentLogIn.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
