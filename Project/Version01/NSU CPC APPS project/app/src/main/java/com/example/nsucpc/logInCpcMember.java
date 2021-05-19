edffa5d118831cbe76a0324cde51e8cca6a084ef
saiful Islam <58458609+Saiful1721648@users.noreply.github.com>
2021-05-20 00:55:31 +0600

    #38 Cpc Sign Up

--- Project/Version01/NSU CPC APPS project/app/src/main/java/com/example/nsucpc/logInCpcMember.java
+++ Project/Version01/NSU CPC APPS project/app/src/main/java/com/example/nsucpc/logInCpcMember.java
@@ -2,13 +2,29 @@ package com.example.nsucpc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class logInCpcMember extends AppCompatActivity {
+public class logInCpcMember extends AppCompatActivity implements View.OnClickListener {
+    private TextView cpcMemberLoginActReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_cpc_member);
+        cpcMemberLoginActReg = (TextView) findViewById(R.id.cpcMemberLoginActReg);
+        cpcMemberLoginActReg.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.cpcMemberLoginActReg:
+                startActivity(new Intent(this, CpcmemberSignUpActivity.class));
+                break;
+
+        }
     }
 }
