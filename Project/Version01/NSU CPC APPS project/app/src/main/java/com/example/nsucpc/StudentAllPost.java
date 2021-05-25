b0bd03835985ecc9e7cd4dd0b57c104493105425
saiful Islam <58458609+Saiful1721648@users.noreply.github.com>
2021-05-26 05:11:31 +0600

    #48 Student Portal All post Card View

---
+++ Project/Version01/NSU CPC APPS project/app/src/main/java/com/example/nsucpc/StudentAllPost.java
@@ -0,0 +1,14 @@
+package com.example.nsucpc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentAllPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_all_post);
+    }
+}
