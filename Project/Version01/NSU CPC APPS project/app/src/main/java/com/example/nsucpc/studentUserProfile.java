77139d09c3ecaec83bfeabc1a089b7b761aed744
saiful Islam <58458609+Saiful1721648@users.noreply.github.com>
2021-05-27 00:31:43 +0600

    #51 Student Portal User Details

---
+++ Project/Version01/NSU CPC APPS project/app/src/main/java/com/example/nsucpc/studentUserProfile.java
@@ -0,0 +1,14 @@
+package com.example.nsucpc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class studentUserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_user_profile);
+    }
+}
