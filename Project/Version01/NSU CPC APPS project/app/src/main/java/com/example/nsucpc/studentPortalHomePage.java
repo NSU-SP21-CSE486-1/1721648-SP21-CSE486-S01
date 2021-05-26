657e65210eac7d148203adec155cef70ea77f6d8
saiful Islam <58458609+Saiful1721648@users.noreply.github.com>
2021-05-27 01:49:29 +0600

    #52 Student Home Page

--- Project/Version01/NSU CPC APPS project/app/src/main/java/com/example/nsucpc/studentPortalHomePage.java
+++ Project/Version01/NSU CPC APPS project/app/src/main/java/com/example/nsucpc/studentPortalHomePage.java
@@ -2,15 +2,44 @@ package com.example.nsucpc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 
 public class studentPortalHomePage extends AppCompatActivity {
-    private ImageButton allPostbutton,logoutButton,appliedJobButton;
+    private ImageButton allPostbutton,logoutButton,appliedJobButton,userProfileHomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_portal_home_page);
+        allPostbutton=(ImageButton)findViewById(R.id.viewallPostHome);
+        logoutButton=(ImageButton)findViewById(R.id.logOutImgButton);
+        appliedJobButton=(ImageButton)findViewById(R.id.appliedimgBtn);
+        userProfileHomeButton=(ImageButton)findViewById(R.id.userprofileButton);
+
+        allPostbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(studentPortalHomePage.this,StudentAllPost.class);
+                startActivity(intent);
+
+            }
+        });
+
+        userProfileHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(studentPortalHomePage.this,studentUser.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+
     }
 }
